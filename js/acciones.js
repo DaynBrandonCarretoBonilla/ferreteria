35256f0d4f8bb427a218eaa6d124ec97f2025edf
DaynBrandonCarretoBonilla <damcb164e9@gmail.com>
2015-12-02 14:19:25 -0600

    ferre8

--- js/acciones.js
+++ js/acciones.js
@@ -11,11 +11,11 @@ document.addEventListener("deviceready",function(){
 			success: function(respuesta){
 				
 			var datosProducto = JSON.parse(respuesta);
-			alert(datosProducto.productos[0].nombreProducto);
+			//alert(datosProducto.productos[0].nombreProducto);
 		
 			for (var x= 0; x<datosProducto.productos.length; x++)
 			{
-				$('#contenido').append('<div><div><div style="display:inline-block"><img src="#"></div><div style="display:inline-block"><h1>'+ datosProducto.productos[x].nombreProducto +' </h1><h2> '+datosProducto.productos[x].marcaProducto+' </h2></div></div><div><h2> '+datosProducto.productos[x].precioUnitario+' </h2><h2> '+datosProducto.productos[x].precioMayoreo+' </h2></div></div>');
+				$('#contenido').append('<div><div><div style="display:inline-block"><img src="http://192.168.1.192/ferreteria/recursos/imagenes/fotos/'+datosProducto.productos[x].claveProducto+'.jpg"></div><div style="display:inline-block"><h1>'+ datosProducto.productos[x].nombreProducto +' </h1><h2> '+datosProducto.productos[x].marcaProducto+' </h2></div></div><div><h2> '+datosProducto.productos[x].precioUnitario+' </h2><h2> '+datosProducto.productos[x].precioMayoreo+' </h2></div></div>');
 			}
 				$(':mobile-pagecontainer').pagecontainer('change', '#PaginaMI',{
 				transition: 'pop'
